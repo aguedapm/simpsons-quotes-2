bc9fd333d1e06f74b5b8ca1b8fe2be1c834b736e
Agueda Perez <aguedapm@yahoo.com>
2019-04-01 14:55:37 +0200

    Reduced header and changed color, added some padding to body

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import GenerateQuotes from './components/GenerateQuotes'
 import DisplayQuote from './components/DisplayQuote'
 
-const sampleSimpson = {
-  quote: "Nothing you say can upset us. We're the MTV generation.",
-  character: "Bart Simpson",
-  image:"https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FBartSimpson.png?1497567511638"
-}
 
 class App extends Component {
 
   state = {
-    simpson : sampleSimpson
+    simpson : {
+      quote: "Nothing you say can upset us. We're the MTV generation.",
+      character: "Bart Simpson",
+      image:"https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FBartSimpson.png?1497567511638"
+    }
   };
 
   getQuote() {
@@ -30,18 +28,9 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1>Simpsons quotes - the return</h1>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
-        <body>
+        <body className="App-body">
           <GenerateQuotes selectQuote={() =>  this.getQuote()} />
           <DisplayQuote simpson={this.state.simpson}/>
         </body>
